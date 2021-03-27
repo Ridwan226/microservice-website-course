b5f3b7ab19c7c06357feaa378b2fb1475dbfb100
Ridwan226 <4515210063@univpancasila.ac.id>
2021-03-28 01:36:32 +0700

    Integrasi dengan API Gateway POST

---
+++ api-gateway/routes/heandler/media/create.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const apiAdapter = require('../../apiAdapter');
+const {URL_SERVICE_MEDIA} = process.env;
+
+const api = apiAdapter();
+
+module.exports = async (req, res) => {
+  try {
+    const media = await api.post(`${URL_SERVICE_MEDIA}/media`, req.body);
+    return res.json(media.data);
+  } catch (error) {
+    if (error.code === 'ECONNREFUSED') {
+      return res
+        .status(500)
+        .json({status: 'error', message: 'Service Tidak Tersedia'});
+    }
+
+    const {status, data} = error.response;
+    return res.status(status).json(data);
+  }
+};
