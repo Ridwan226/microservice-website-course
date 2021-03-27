42d992f5a623e063d9ef7f67ce97a87b5d6605d2
Ridwan226 <4515210063@univpancasila.ac.id>
2021-03-27 23:09:57 +0700

    Delete Media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 const {Media} = require('../models');
+const fs = require('fs');
+
+router.get('/', async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image'],
+  });
+
+  const meappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status: 'success',
+    data: meappedMedia,
+  });
+});
 
 /* post users listing. */
 router.post('/', function (req, res, next) {
@@ -18,7 +35,7 @@ router.post('/', function (req, res, next) {
     const path = require('path');
     const filename = path.parse(filepath).base;
 
-    const media = await Media.create({image: `image/${filename}`});
+    const media = await Media.create({image: `images/${filename}`});
 
     return res.json({
       status: 'success',
@@ -30,6 +47,30 @@ router.post('/', function (req, res, next) {
   });
 
   // return res.send('oke');
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res
+      .status(400)
+      .json({status: 'error', massage: 'Data Tidak Tersedia'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({status: 'error', massage: err.message});
+    }
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      massage: 'image Deleted',
+    });
+  });
 });
 
 module.exports = router;
